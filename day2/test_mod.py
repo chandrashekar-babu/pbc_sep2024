0adc4b2dddd57f04f75f1d57dd8e3ae483cc0e9e
= <=>
2024-09-24 15:38:56 +0530

    Day 2 -> upto while loop

---
+++ day2/test_mod.py
@@ -0,0 +1,8 @@
+def square(x):
+    return x*x
+
+user = "John"
+
+place = "Delhi"
+
+print(f"{user=}, {place=}")
---
+++ day2/type_test.py
@@ -0,0 +1,10 @@
+a :int = 100
+b :str = "Hello world"
+
+print(a, type(a))
+print(b, type(b))
+
+a = 4.5
+b = [11, 22, 33]
+print(a, type(a))
+print(b, type(b))
