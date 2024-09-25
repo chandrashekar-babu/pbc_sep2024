d1f9e8e2b3bb146a17b067c1586a63a89ff5f57f
= <=>
2024-09-25 17:39:47 +0530

    List operations

---
+++ day3/mathtools.py
@@ -0,0 +1,18 @@
+a = 100
+"""A simple math utility library
+
+   This module provides a simple set of functions/classes for
+   math operations.
+"""
+
+def square(x):
+    v = 0
+    """Returns square of x"""
+    return x*x
+
+def sqrt(n):
+    """Returns square root of n"""
+    return n ** 0.5
+
+class Point:
+    "A class that represent Point coordinates"
